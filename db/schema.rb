da6192c10438be2f2de595c2b15e201499278d3e
nagai.hironori <nagai.hironori@techacademy.jp>
2019-10-07 07:47:04 +0000

    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_031600) do
+ActiveRecord::Schema.define(version: 2019_10_03_140451) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2019_10_07_031600) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.bigint "user_id"
-    t.string "user"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
